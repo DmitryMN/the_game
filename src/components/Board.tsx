3ea259eba98926c52bef1621f1c22ac548a4256d
MalenkovDN <m18dm.dm@gmail.com>
2022-01-13 22:54:36 +0300

    add calculateWinner function and refactoring

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,19 +1,24 @@
+import { type } from "os";
 import React from "react";
 import Square from "./Square";
 
-export type SquaresType = string | null
+export type SquaresType = string
 
 interface BoardState {
     squares: Array<SquaresType>
     xIsNext: boolean
 }
 
+type ValueLines = Array<number>
+
+type LinesType = Array<ValueLines>
+
 class Board extends React.Component<{}, BoardState> {
 
     constructor(props: any) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null),
+            squares: Array(9).fill(''),
             xIsNext: true,
         }
     }
@@ -32,7 +37,8 @@ class Board extends React.Component<{}, BoardState> {
     }
 
     render() {
-        const status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
+        const winner = calculateWinner(this.state.squares);
+        let status = winner ? `Winner: ${winner}` : `Next move ${this.state.xIsNext ? 'X' : 'O'}`
 
         return (
             <div>
@@ -55,6 +61,27 @@ class Board extends React.Component<{}, BoardState> {
             </div>
         );
     }
+}
+
+const calculateWinner = (squares: Array<SquaresType>) => {
+    const lines: LinesType = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    
+    for(let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    return '';
 }
 
 export default Board;
